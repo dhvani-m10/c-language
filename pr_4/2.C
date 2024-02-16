26a859999fea3a76ca324bc93797361e5ae10c64
dhvani-m10 <dhvanimoradiya10@gmail.com>
2024-02-16 22:16:02 +0530

    16/02/2024

---
+++ pr_4/2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j,n=0;
+	clrscr();
+
+	for(i=1;i<=5;i++)
+	{
+	  for(j=1;j<=i;j++)
+	  {
+		n=n+11;
+		printf("%d ",n);
+	  }
+	   printf("\n");
+	}
+
+	getch();
+}
