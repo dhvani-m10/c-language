dcfdb676538e25c01ace19c2007dd5b953a33b4e
dhvani-m10 <dhvanimoradiya10@gmail.com>
2024-02-18 13:36:24 +0530

    18/02/2024

---
+++ ch_11/11_2/10.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j,s;
+	clrscr();
+
+	for(i=1;i<=5;i++)
+	{
+	  for(s=5;s>i;s--)
+	  {
+		printf(" ");
+	  }
+	  for(j=1;j<=i-1;j++)
+	  {
+		printf("%d",j);
+	  }
+	  for(j=i;j>=1;j--)
+	  {
+		printf("%d",j);
+	  }
+	  printf("\n");
+	}
+
+	getch();
+}
